15ea15df2febe34ed2129f80a94d985acd3ee2fd
lara huang <lara1105huang@gmail.com>
2023-09-13 13:16:03 +0800

    新增 element-plus 自動導入

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,22 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { resolve } from 'path'
-
+import AutoImport from 'unplugin-auto-import/vite';/*element 自動導入*/
+import Components from 'unplugin-vue-components/vite';/*element 自動導入*/
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';/**/
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [
-    vue()
+    vue(),
+    /*element 自動導入*/
+    AutoImport({
+     resolvers: [ElementPlusResolver()],
+    }),
+    /*element 自動導入*/
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
     alias: {
